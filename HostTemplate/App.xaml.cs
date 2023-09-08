0a86cbeb30f901c393dd2e45045a57591b1bb5f3
Artur Dias <arturdrm@hotmail.com>
2023-09-08 08:32:01 -0300

    First commit

---
+++ HostTemplate/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Windows;
+
+namespace HostTemplate;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private IHost _host;
+
+    public App()
+    {
+        _host = new HostBuilder()
+            .Build();
+    }
+
+    protected override async void OnStartup(StartupEventArgs e)
+    {
+       await _host.StartAsync();
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        using (_host)
+        {
+            await _host.StopAsync(TimeSpan.FromSeconds(5));
+        }
+    }
+}
