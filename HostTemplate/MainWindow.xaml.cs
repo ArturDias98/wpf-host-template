58d604581b43bd38b923c7df0a1ede519a5d19ec
Artur Dias <arturdrm@hotmail.com>
2023-09-08 09:43:25 -0300

    Binding account settings in main window

--- HostTemplate/MainWindow.xaml.cs
+++ HostTemplate/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using HostTemplate.Options;
 using Microsoft.Extensions.Options;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
 
 namespace HostTemplate;
@@ -7,8 +9,10 @@ namespace HostTemplate;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : Window, INotifyPropertyChanged
 {
+    private string _userName = string.Empty;
+    private string _email = string.Empty;
     private readonly AccountSettings _accountSettings;
 
     public MainWindow(IOptions<AccountSettings> options)
@@ -16,5 +20,42 @@ public partial class MainWindow : Window
         _accountSettings = options.Value;
 
         InitializeComponent();
+
+        DataContext = this;
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        UserName = _accountSettings.UserName;
+        Email = _accountSettings.Email;
+    }
+
+    public string UserName
+    {
+        get { return _userName; }
+        set
+        {
+            _userName = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Email
+    {
+        get { return _email; }
+        set
+        {
+            _email = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
